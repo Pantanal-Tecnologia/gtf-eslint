c184ce00a7f578ffd8789bfe3048a736cc9d7983
thethiago27 <thethiago27@gmail.com>
2025-07-01 09:29:13 -0400

    **Add ESLint configuration with Prettier integration and initial setup**

---
+++ node.js
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  ignores: ["/dist"],
+  extends: [
+    eslintPluginPrettierRecommended,
+    js.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.strictTypeChecked,
+  ],
+  files: ["**/*.{ts}"],
+  languageOptions: {
+    ecmaVersion: 2022,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        trailingComma: "all",
+        arrowParens: "always",
+        bracketSpacing: true,
+        bracketSameLine: false,
+        semi: true,
+        endOfLine: "crlf",
+        quoteProps: "as-needed",
+        embeddedLanguageFormatting: "auto",
+        proseWrap: "preserve",
+        htmlWhitespaceSensitivity: "css",
+        vueIndentScriptAndStyle: false,
+        jsxSingleQuote: false,
+        insertPragma: false,
+        requirePragma: false,
+      },
+    ],
+  },
+});
